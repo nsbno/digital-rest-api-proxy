ee70d7b6bd308d41f01cdd90eb794223d05b8cd3
Tom Arne <tom.brandvold@vy.no>
2026-06-16 16:27:53 +0200

    Remove commented-out code

--- main.tf
+++ main.tf
@@ -33,35 +33,7 @@ module "api_proxy_addon_module" {
       "integration.request.header.host" = "'${var.service_name}.${data.aws_route53_zone.internal_vydev_io_zone_name.name}'"
     }
   }
-  
-  
-  
-  # uri = "https://${var.service_name}.${data.aws_route53_zone.internal_vydev_io_zone_name.name}/{proxy}"
-  # connection_id = data.aws_ssm_parameter.apigw_vpc_link_id.value
-  # integration_target = local.shared_config.lb_internal_arn
-  
-  # request_parameters = {
-  #   "integration.request.path.proxy"  = "method.request.path.proxy"
-  #   "integration.request.header.host" = "'${var.service_name}.${data.aws_route53_zone.internal_vydev_io_zone_name.name}'"
-  # }
 }
-
-# resource "aws_api_gateway_integration" "rest_service" {
-#   rest_api_id             = module.general_rest_api_module.rest_api_id
-#   resource_id             = module.api_proxy_addon_module.recource_proxy_id
-#   http_method             = module.api_proxy_addon_module.http_method
-#   type                    = "HTTP_PROXY"
-#   integration_http_method = "ANY"
-#   # uri                     = "https://${var.service_name}.${data.aws_route53_zone.internal_vydev_io_zone_name.name}/{proxy}"
-#   connection_type         = "VPC_LINK"
-#   # connection_id           = data.aws_ssm_parameter.apigw_vpc_link_id.value
-#   # integration_target      = local.shared_config.lb_internal_arn
-
-#   # request_parameters = {
-#   #   "integration.request.path.proxy"  = "method.request.path.proxy"
-#   #   "integration.request.header.host" = "'${var.service_name}.${data.aws_route53_zone.internal_vydev_io_zone_name.name}'"
-#   # }
-# }
 
 resource "aws_apigatewayv2_api_mapping" "service" {
   api_id      = module.general_rest_api_module.rest_api_id
@@ -76,32 +48,3 @@ resource "aws_wafv2_web_acl_association" "rest_service" {
   resource_arn = module.general_rest_api_module.stage_arn
   web_acl_arn  = data.aws_ssm_parameter.rest_api_waf_arn.value
 }
-
-
-
-
-# resource "aws_api_gateway_deployment" "rest_service" {
-#   rest_api_id = aws_api_gateway_rest_api.rest_service.id
-
-#   triggers = {
-#     redeployment = sha1(jsonencode([
-#       aws_api_gateway_resource.rest_service_proxy.id,
-#       aws_api_gateway_resource.rest_service_proxy.path_part,
-#       aws_api_gateway_method.rest_service_any.id,
-#       aws_api_gateway_method.rest_service_any.http_method,
-#       aws_api_gateway_method.rest_service_any.request_parameters,
-#       aws_api_gateway_integration.rest_service.id,
-#       aws_api_gateway_integration.rest_service.type,
-#       aws_api_gateway_integration.rest_service.uri,
-#       aws_api_gateway_integration.rest_service.connection_type,
-#       aws_api_gateway_integration.rest_service.request_parameters,
-#     ]))
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [aws_api_gateway_integration.rest_service]
-# }
-
