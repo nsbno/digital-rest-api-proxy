fa0775ee5d6d7f9fb16872cd9e5b42b8b714a323
Tom Arne <tom.brandvold@vy.no>
2026-06-19 11:48:58 +0200

    Update module source references to version 1.0.0

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "general_rest_api_module" {
-  source = "github.com/nsbno/terraform-aws-rest-api?ref=rest-api"
+  source = "github.com/nsbno/terraform-aws-rest-api?ref=1.0.0"
 
   name = var.service_name
 
@@ -17,7 +17,7 @@ module "general_rest_api_module" {
   })
 }
 module "api_proxy_addon_module" {
-  source = "github.com/nsbno/terraform-aws-rest-api//modules/proxy-api?ref=rest-api"
+  source = "github.com/nsbno/terraform-aws-rest-api//modules/proxy-api?ref=1.0.0"
 
   rest_api_id = module.general_rest_api_module.rest_api_id
   parent_id   = module.general_rest_api_module.root_resource_id
