120b303b8ab95caa700e1a9be3fa09b19dffe1ec
Tom Arne <tom.brandvold@vy.no>
2026-06-19 12:59:42 +0200

    Use local for application_name in example

--- examples/springboot-service-with-rest-api/main.tf
+++ examples/springboot-service-with-rest-api/main.tf
@@ -1,10 +1,13 @@
+locals {
+  application_name = "my-application-name"
+}
 # module "spring_boot_service" {
 #   source                                = "github.com/nsbno/terraform-digitalekanaler-modules//spring-boot-service?ref=x.y.z"
-#   name                                  = "my-application-name"
+#   name                                  = local.application_name
 #   ...
 # }
 
 module "digital_rest_api_proxy" {
   source       = "github.com/nsbno/digital-rest-api-proxy?ref=genesis"
-  service_name = "my-application-name"
+  service_name = local.application_name
 }
