b7a26fcf5f754e218977bfe2079ee9e2527d9547
Tom Arne <tom.brandvold@vy.no>
2026-06-19 14:55:09 +0200

    Fix description in variables.tf for service_name and custom_api_gateway_path consistency

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
 variable "service_name" {
   type = string
+  description = "Name of the service used in the API gateway service path. Your service will be avaialable at /services/<service_name>"
 }
 
 variable "custom_api_gateway_path" {
   type        = string
   default     = null
-  description = "By default, your service will be avaialable at /services/<name>. If you set this variable, it will be available at /services/<custom_api_gateway_path> intead."
+  description = "By default, your service will be avaialable at /services/<service_name>. If you set this variable, it will be available at /services/<custom_api_gateway_path> intead."
 }
