caab33a546d4b0e27c6db46164a2b1a1313282fa
Tom Arne <tom.brandvold@vy.no>
2026-06-19 11:59:13 +0200

    Remove commented-out variable definitions from variables.tf

--- variables.tf
+++ variables.tf
@@ -2,14 +2,6 @@ variable "service_name" {
   type = string
 }
 
-# variable "hosted_zone_name" {
-#     type = string
-# }
-
-# variable "alb_arn" {
-#     type = string
-# }
-
 variable "custom_api_gateway_path" {
   type        = string
   default     = null
