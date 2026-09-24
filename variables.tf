d93e6604856fa8f82172e5868075dd32531caee9
Mohamad Kenas <mohamad.kenas@vy.no>
2026-09-24 15:27:29 +0200

    Add support for binary media types

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "service_name" {
-  type = string
+  type        = string
   description = "Name of the service used in the API gateway service path. Your service will be avaialable at /services/<service_name>"
 }
 
@@ -10,7 +10,22 @@ variable "custom_api_gateway_path" {
 }
 
 variable "waf_enabled" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "By default, WAF keeps the API private and only reachable from CloudFront and main-frontend. Disabling it makes the endpoint publicly available and more vulnerable to DoS attacks."
+}
+
+variable "binary_media_types" {
+  description = "MIME types that API Gateway should treat as binary data."
+  type        = list(string)
+  default     = []
+}
+
+variable "method_request_parameters" {
+  description = "Request parameters to be passed from the method request to the integration request."
+  type        = map(bool)
+  default = {
+    "method.request.path.proxy"  = true
+    "method.request.header.host" = true
+  }
 }
